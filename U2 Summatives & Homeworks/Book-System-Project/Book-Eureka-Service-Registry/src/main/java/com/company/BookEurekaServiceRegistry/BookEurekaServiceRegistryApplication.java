64c47fceb61c1120bd482f4db06ebc1917a1ed26
Elicas5931 <elizabeth.perry.339@gmail.com>
2019-08-06 10:12:59 -0400

    Adding Book Project

---
+++ U2 Summatives & Homeworks/Book-System-Project/Book-Eureka-Service-Registry/src/main/java/com/company/BookEurekaServiceRegistry/BookEurekaServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.company.BookEurekaServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEurekaServiceRegistryApplication.class, args);
+	}
+
+}
